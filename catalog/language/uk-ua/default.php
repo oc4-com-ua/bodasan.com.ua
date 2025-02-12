17ac484b76b7a4bae6a9ec254ac0815edfac4cec
oc4-com-ua <oc4.com.ua@gmail.com>
2025-02-12 22:27:08 +0200

    Added Ukrainian language files (partial translation)

---
+++ catalog/language/uk-ua/default.php
@@ -0,0 +1,90 @@
+<?php
+// Locale
+$_['code']                  = 'uk';
+$_['direction']             = 'ltr';
+$_['date_format_short']     = 'd.m.Y';
+$_['date_format_long']      = 'l, d F Y';
+$_['time_format']           = 'H:i:s';
+$_['datetime_format']       = 'd.m.Y H:i:s';
+$_['decimal_point']         = '.';
+$_['thousand_point']        = ',';
+
+// Text
+$_['text_home']             = 'Головна';
+$_['text_yes']              = 'Так';
+$_['text_no']               = 'Ні';
+$_['text_none']             = ' — Немає — ';
+$_['text_select']           = ' — Оберіть — ';
+$_['text_all_zones']        = 'Усі зони';
+$_['text_pagination']       = 'Показано %d–%d із %d (сторінок: %d)';
+$_['text_loading']          = 'Завантаження...';
+$_['text_no_results']       = 'Нічого не знайдено!';
+$_['text_just_now']         = 'щойно';
+$_['text_seconds_ago']      = '%s секунд тому';
+$_['text_minute_ago']       = '%s хвилину тому';
+$_['text_minutes_ago']      = '%s хвилин тому';
+$_['text_hour_ago']         = '%s годину тому';
+$_['text_hours_ago']        = '%s годин тому';
+$_['text_day_ago']          = '%s день тому';
+$_['text_days_ago']         = '%s днів тому';
+$_['text_week_ago']         = '%s тиждень тому';
+$_['text_weeks_ago']        = '%s тижнів тому';
+$_['text_month_ago']        = '%s місяць тому';
+$_['text_months_ago']       = '%s місяців тому';
+$_['text_year_ago']         = '%s рік тому';
+$_['text_years_ago']        = '%s років тому';
+
+// Buttons
+$_['button_address_add']    = 'Додати адресу';
+$_['button_back']           = 'Назад';
+$_['button_continue']       = 'Продовжити';
+$_['button_cart']           = 'Додати в кошик';
+$_['button_cancel']         = 'Скасувати';
+$_['button_compare']        = 'Порівняти цей товар';
+$_['button_wishlist']       = 'Додати до списку бажань';
+$_['button_checkout']       = 'Оформити замовлення';
+$_['button_confirm']        = 'Підтвердити замовлення';
+$_['button_coupon']         = 'Застосувати купон';
+$_['button_delete']         = 'Видалити';
+$_['button_download']       = 'Завантажити';
+$_['button_edit']           = 'Редагувати';
+$_['button_filter']         = 'Уточнити';
+$_['button_new_address']    = 'Нова адреса';
+$_['button_change_address'] = 'Змінити адресу';
+$_['button_reviews']        = 'Відгуки';
+$_['button_write']          = 'Написати відгук';
+$_['button_login']          = 'Увійти';
+$_['button_update']         = 'Оновити';
+$_['button_remove']         = 'Видалити';
+$_['button_reorder']        = 'Купити знову';
+$_['button_return']         = 'Повернути товар';
+$_['button_shopping']       = 'Продовжити покупки';
+$_['button_search']         = 'Пошук';
+$_['button_submit']         = 'Надіслати';
+$_['button_guest']          = 'Оформити без реєстрації';
+$_['button_view']           = 'Переглянути';
+$_['button_upload']         = 'Завантажити файл';
+$_['button_reward']         = 'Застосувати бали';
+$_['button_choose']         = 'Вибрати';
+$_['button_shipping']       = 'Застосувати доставку';
+$_['button_quote']          = 'Отримати розрахунок';
+$_['button_list']           = 'Список';
+$_['button_grid']           = 'Сітка';
+$_['button_map']            = 'Переглянути на Google Картах';
+
+// Error
+$_['error_exception']       = 'Код помилки (%s): %s у файлі %s на рядку %s';
+$_['error_upload_1']        = 'Увага: Завантажений файл перевищує максимальний розмір, встановлений у php.ini!';
+$_['error_upload_2']        = 'Увага: Завантажений файл перевищує обмеження MAX_FILE_SIZE, вказане у HTML-формі!';
+$_['error_upload_3']        = 'Увага: Файл був завантажений лише частково!';
+$_['error_upload_4']        = 'Увага: Файл не було завантажено!';
+$_['error_upload_6']        = 'Увага: Відсутня тимчасова папка!';
+$_['error_upload_7']        = 'Увага: Не вдалося записати файл на диск!';
+$_['error_upload_8']        = 'Увага: Завантаження файлу зупинено розширенням!';
+$_['error_upload_999']      = 'Увага: Код помилки відсутній!';
+$_['error_upload_size']     = 'Увага: Завантажений файл перевищує максимальний розмір %s МБ!';
+$_['error_curl']            = 'CURL: Код помилки (%s): %s';
+$_['error_session']         = 'Увага: Сесія закінчилася, будь ласка, надішліть форму знову!';
+
+// Datepicker
+$_['datepicker']            = 'uk';
