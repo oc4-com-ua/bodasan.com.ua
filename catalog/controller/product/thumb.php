85dfb1c9122733e2f309d3cb7b6b9756d77c1a5e
oc4-com-ua <oc4.com.ua@gmail.com>
2025-03-02 18:29:04 +0200

    Added stock status to product thumbnail template data

--- catalog/controller/product/thumb.php
+++ catalog/controller/product/thumb.php
@@ -16,6 +16,24 @@ class Thumb extends \Opencart\System\Engine\Controller {
 	public function index(array $data): string {
 		$this->load->language('product/thumb');
 
+        $this->load->model('localisation/stock_status');
+
+        if ($data['quantity'] <= 0) {
+            $stock_status_id = $data['stock_status_id'];
+        } elseif (!$this->config->get('config_stock_display')) {
+            $stock_status_id = (int)$this->config->get('config_stock_status_id');
+        } else {
+            $stock_status_id = 0;
+        }
+
+        $stock_status_info = $this->model_localisation_stock_status->getStockStatus($stock_status_id);
+
+        if ($stock_status_info) {
+            $data['stock'] = $stock_status_info['name'];
+        } else {
+            $data['stock'] = $data['quantity'];
+        }
+
 		$data['cart'] = $this->url->link('common/cart.info', 'language=' . $this->config->get('config_language'));
 
 		$data['cart_add'] = $this->url->link('checkout/cart.add', 'language=' . $this->config->get('config_language'));
