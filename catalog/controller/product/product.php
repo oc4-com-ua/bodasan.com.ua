a71362e7999580e0e95512ac5f06c2a22b014653
oc4-com-ua <oc4.com.ua@gmail.com>
2025-03-05 14:09:22 +0200

    refactor: remove outdated magnificPopup from product gallery

--- catalog/controller/product/product.php
+++ catalog/controller/product/product.php
@@ -233,9 +233,6 @@ class Product extends \Opencart\System\Engine\Controller {
 			$this->document->setKeywords($product_info['meta_keyword']);
 			$this->document->addLink($this->url->link('product/product', 'language=' . $this->config->get('config_language') . '&product_id=' . $product_id), 'canonical');
 
-			$this->document->addScript('catalog/view/javascript/jquery/magnific/jquery.magnific-popup.min.js');
-			$this->document->addStyle('catalog/view/javascript/jquery/magnific/magnific-popup.css');
-
 			$data['heading_title'] = $product_info['name'];
 
 			$data['text_minimum'] = sprintf($this->language->get('text_minimum'), $product_info['minimum']);
