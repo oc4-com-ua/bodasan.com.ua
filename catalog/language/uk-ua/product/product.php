a7f4c4ff12f85293045c39317f2fa51380a7a789
oc4-com-ua <oc4.com.ua@gmail.com>
2025-03-06 11:46:37 +0200

    refactor: moved manufacturer display to product attributes table and removed link

--- catalog/language/uk-ua/product/product.php
+++ catalog/language/uk-ua/product/product.php
@@ -2,7 +2,7 @@
 // Текст
 $_['text_search']                = 'Пошук';
 $_['text_brand']                 = 'Бренд';
-$_['text_manufacturer']          = 'Бренд:';
+$_['text_manufacturer']          = 'Бренд';
 $_['text_model']                 = 'Код продукту:';
 $_['text_reward']                = 'Бонусні бали:';
 $_['text_points']                = 'Ціна у бонусних балів:';
